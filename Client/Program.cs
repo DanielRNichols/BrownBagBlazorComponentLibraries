5c47ec53b1ca12d58079e04cfffc0713cf02fc6b
Daniel Nichols <danielrnichols63@gmail.com>
2022-02-16 09:54:36 -0600

    Manual installation of MudBlazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using MudBlazorGolfers.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add MudBlazor
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
